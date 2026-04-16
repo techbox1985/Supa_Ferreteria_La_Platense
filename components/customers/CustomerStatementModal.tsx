2642c9a06d770b125fbd59341ea6d75bc225ae31
techbox1985 <techbox.argentina@gmail.com>
2026-04-15 21:51:23 -0300

    add delete action for credit notes in customer statement

--- components/customers/CustomerStatementModal.tsx
+++ components/customers/CustomerStatementModal.tsx
@@ -322,6 +322,9 @@ export const CustomerStatementModal: React.FC<CustomerStatementModalProps> = ({ 
             } else if (txToDelete.type === 'Pago' && txToDelete.id) {
                 await api.deletePayment(txToDelete.id);
                 addToast('Pago eliminado con éxito.', 'success');
+            } else if (txToDelete.type === 'Nota de Crédito' && txToDelete.id) {
+                await api.deleteAccountTransactionById(txToDelete.id);
+                addToast('Nota de crédito eliminada con éxito.', 'success');
             }
             refreshData();
         } catch (error) {
@@ -421,11 +424,11 @@ export const CustomerStatementModal: React.FC<CustomerStatementModalProps> = ({ 
                                                                 <Icon path="M2.036 12.322a1.012 1.012 0 010-.639l4.418-5.523A1.012 1.012 0 017.5 6h9a1.012 1.012 0 01.946.689l4.418 5.523a1.012 1.012 0 010 .639l-4.418 5.523A1.012 1.012 0 0116.5 18h-9a1.012 1.012 0 01-.946-.689L2.036 12.322zM15 12a3 3 0 11-6 0 3 3 0 016 0z" className="w-5 h-5"/>
                                                             </button>
                                                         )}
-                                                        {isAdmin && ((tx.type === 'Venta' && tx.originalSaleId) || tx.type === 'Pago') && (
+                                                        {isAdmin && ((tx.type === 'Venta' && tx.originalSaleId) || tx.type === 'Pago' || tx.type === 'Nota de Crédito') && (
                                                             <button
                                                                 onClick={() => handleDeleteRequest(tx)}
                                                                 className="text-red-600 hover:text-red-800"
-                                                                title={tx.type === 'Venta' ? 'Anular esta venta' : 'Eliminar este pago'}
+                                                                title={tx.type === 'Venta' ? 'Anular esta venta' : tx.type === 'Pago' ? 'Eliminar este pago' : 'Eliminar esta nota de crédito'}
                                                             >
                                                                 <Icon path="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" className="w-5 h-5"/>
                                                             </button>
